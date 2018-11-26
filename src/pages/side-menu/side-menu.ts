e2199fbc88e72bf3cbd2abf3124861353e72d5c6
Barbero95 <barberodavid95@yahoo.es>
2018-11-26 15:40:47 +0100

    sideMenu y perfil

--- src/pages/side-menu/side-menu.ts
+++ src/pages/side-menu/side-menu.ts
@@ -3,12 +3,14 @@ import { Platform, MenuController, Nav, IonicPage, NavController, NavParams } fr
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { Storage } from '@ionic/storage';
+import { AlertController } from 'ionic-angular';
 
 import { CatalogoPage } from '../catalogo/catalogo';
 import { MenuPrincipalPage } from '../menu-principal/menu-principal';
 import { CrearActividadPage } from '../crear-actividad/crear-actividad';
 import { PerfilPage } from '../perfil/perfil';
 import { FrontendServicesProvider } from '../../providers/frontend-services/frontend-services';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the SideMenuPage page.
@@ -27,19 +29,20 @@ export class SideMenuPage {
   @ViewChild(Nav) nav: Nav;
   rootPage: any = MenuPrincipalPage;
   pages: Array<{title: string, component: any, icon: string}>;
-
+  pageExit: {title: string, component: any, icon: string}
   propietario: string = "";
   nick: string = "";
   estrellas: number = 0;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public menu: MenuController,public statusBar: StatusBar,
-    public splashScreen: SplashScreen, public platform: Platform, private frontendServices: FrontendServicesProvider, public storage: Storage) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public menu: MenuController,public statusBar: StatusBar, 
+    public splashScreen: SplashScreen, public platform: Platform, private frontendServices: FrontendServicesProvider, public storage: Storage,public alertCtrl: AlertController) {
       //this.initializeApp();
       this.pages = [
       { title: 'Menu Principal', component: MenuPrincipalPage, icon:"home" },
       { title: 'Catálogo', component: CatalogoPage, icon: "book"},
       { title: 'Crear Actividad', component: CrearActividadPage, icon:"add-circle"}
       ];
+      this.pageExit = { title: 'LogOut', component: HomePage, icon: "exit" };
 
     this.storage.get('nick').then( (propietario) => {
       //this.propietario = val;
@@ -80,6 +83,32 @@ export class SideMenuPage {
       //this.estrellas = data.estrellas;
       this.estrellas = 3.2;
     });
+  }
+  goExit(){
+    this.showConfirm();
+  }
+  showConfirm() {
+    const confirm = this.alertCtrl.create({
+      title: 'LogOut',
+      message: '¿Estas seguro que quieres salir de la aplicación?',
+      buttons: [
+        {
+          text: 'Disagree',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Agree',
+          handler: () => {
+            console.log('Agree clicked');
+            this.menu.close();
+            this.nav.setRoot(HomePage);
+          }
+        }
+      ]
+    });
+    confirm.present();
   }
 
 }
