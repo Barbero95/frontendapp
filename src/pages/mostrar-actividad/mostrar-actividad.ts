432adda7352aae89179bd0326ee6f8c34fd4ffcd
ArnauSotis <arnau.soguero@estudiant.upc.edu>
2018-12-07 13:52:36 +0100

    empeando la interaccion con la actividad para cerrar el circulo

--- src/pages/mostrar-actividad/mostrar-actividad.ts
+++ src/pages/mostrar-actividad/mostrar-actividad.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Actividad } from '../../app/actividad';
+import { AlertController } from 'ionic-angular';
 /**
  * Generated class for the MostrarActividadPage page.
  *
@@ -17,11 +18,45 @@ export class MostrarActividadPage {
 
   actividad: Actividad;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
     this.actividad = new Actividad();
     this.actividad = this.navParams.get('act');
   }
 
+  showAlert1() {
+    const alert = this.alertCtrl.create({
+      title: 'Solicitar actividad',
+      subTitle: '¿Estás seguro de que deseas solicitar una actividad con el usuario que colgó la oferta? Se recomienda contactar antes con él',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancelar',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Aceptar',
+          handler: () => {
+            console.log('Buy clicked');
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+
+
+  contactar(){
+//Aqui se iniciarlizaria un chat que le debería tocar a Bruno
+  }
+  solicitar(){
+    this.showAlert1();
+  }
+  cancelar(){
+    this.navCtrl.pop();
+  }
   
 
 }
